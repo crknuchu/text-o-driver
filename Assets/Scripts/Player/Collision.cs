f24bde8afcd8272a3490a165daf79c60d6a0ac39
Mihailo Bogdanovic <mihailo.bogdanovic1@gmail.com>
2024-01-27 13:26:39 +0100

    implement obstacle collision

---
+++ Assets/Scripts/Player/Collision.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("TEST");
+    }
+}
