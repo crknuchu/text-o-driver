b218f78521a79752f635592236c21cd3d20f91f3
milanceandrejic <milanceandrejic02@gmail.com>
2024-01-27 23:58:33 +0100

    UI

---
+++ Assets/Scripts/Managers/LoadingScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScene : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+
+    public Image loadingImage;
+
+    public void LoadScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        
+        LoadingScreen.SetActive(true);
+        
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
