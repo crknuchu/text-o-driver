81ce7c15dd8c5a5f5638088c8865f65bbf7d6289
Mihailo Bogdanovic <mihailo.bogdanovic1@gmail.com>
2024-01-28 14:31:58 +0100

    add car crash sound

--- Assets/Scripts/Player/Collision.cs
+++ Assets/Scripts/Player/Collision.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class Collision : MonoBehaviour
 {
     public CameraShake cameraShake;
+    public AudioSource audioSource;
+    public AudioClip crash;
     private void OnTriggerEnter(Collider other)
     {
         StartCoroutine(cameraShake.Shake());
         RumbleManager.instance.RumblePulse(0.5f,0.75f,0.4f);
+        audioSource.PlayOneShot(crash,0.5f);
     }
     
 }
